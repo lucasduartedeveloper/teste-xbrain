6b450084bff7e67827577cdec37844381d1c49e8
Lucas Duarte <lucasduartedeveloper@gmail.com>
2022-01-13 17:27:17 -0300

    Add files via upload

---
+++ src/main/java/br/com/xbrain/testebrain/testexbrain/TesteXbrainApplication.java
@@ -0,0 +1,13 @@
+package br.com.xbrain.testebrain.testexbrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteXbrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteXbrainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/xbrain/testebrain/testexbrain/TesteXbrainApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.xbrain.testebrain.testexbrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteXbrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
