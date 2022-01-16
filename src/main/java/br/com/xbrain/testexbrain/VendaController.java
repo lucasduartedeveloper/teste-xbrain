902f6d1754e8d1cfe7d9ec43c22765dca4f970d8
* <lucasduartedeveloper@gmail.com>
2022-01-16 00:52:21 -0300

    first commit

--- src/main/java/br/com/xbrain/testexbrain/VendaController.java
+++ src/main/java/br/com/xbrain/testexbrain/VendaController.java
@@ -3,6 +3,9 @@ import java.util.List;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 @RestController
 @RequestMapping("/api/vendas")
 class VendaController {
